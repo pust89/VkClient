87ce34ed569e9fb579c878286773bdbbb5a0b155
Pustovit Vladimir <vovapust1989@gmail.com>
2024-07-23 18:36:17 +0300

    add real api

--- data_source_impl/src/main/java/com/pustovit/vkclient/data_source_impl/remote/news/mapper/FeedPostMapper.kt
+++ data_source_impl/src/main/java/com/pustovit/vkclient/data_source_impl/remote/news/mapper/FeedPostMapper.kt
@@ -1,6 +1,7 @@
 package com.pustovit.vkclient.data_source_impl.remote.news.mapper
 
 import android.util.Log
+import com.pustovit.vkclient.data_source_impl.remote.news.model.FeedPostDto
 import com.pustovit.vkclient.data_source_impl.remote.news.model.FeedPostResponseDto
 import com.pustovit.vkclient.models.post.FeedPost
 import javax.inject.Inject
@@ -13,9 +14,19 @@ import javax.inject.Inject
 class FeedPostMapper @Inject constructor() {
 
     fun map(dto: FeedPostResponseDto?): List<FeedPost> {
-        dto?.items?.forEach {
-            Log.d("TAG", "dto = $it ")
-        }
-        return emptyList()
+        return dto?.items?.map(::map) ?: emptyList()
+    }
+
+    private fun map(dto: FeedPostDto): FeedPost {
+        return FeedPost(
+            id = dto.id ?: 0,
+            communityName = "communityName",
+            contentText = dto.text.orEmpty(),
+            contentImageUrl = getContentPhotoUrl(dto)
+        )
+    }
+
+    private fun getContentPhotoUrl(dto: FeedPostDto):String{
+        return dto.attachments?.getOrNull(0)?.photo?.sizes?.last()?.url.orEmpty()
     }
 }
